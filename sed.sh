dbf757ebfa39c655b1c68ca9d1e4a1ed5e74e97c
J. Elias Ribeiro Moreira <40527854+jeliasmoreira@users.noreply.github.com>
2019-09-20 15:53:50 -0300

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -1,4 +1,5 @@
 while IFS= read -r line; do 
     sed -i "s/localhost/$line/g" ./conn.php
-    sed -i "s/localhost/$line/g" ./index.html    
+    sed -i "s/localhost/$line/g" ./index.html
+    sed -i "s/localhost/$line/g" ./index.php
 done < "$1"
