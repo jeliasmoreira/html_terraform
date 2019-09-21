a987df3a6fbe4ebfc2f2c1ee1fb1c834d8c57c19
J. Elias Ribeiro Moreira <40527854+jeliasmoreira@users.noreply.github.com>
2019-09-20 22:04:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php
   echo "<div align='left'> <h1> <p> Infraestrutura e aplicação provisionados 100% via Teraform <em>{https://www.terraform.io} </h1></p></div>";
 
-  $link = mysqli_connect("terraform-20190920201311650800000001.cyuraa5fidnz.sa-east-1.rds.amazonaws.com", "sfd2019", "897*&098811qQA");
+  $link = mysqli_connect("localhost", "sfd2019", "897*&098811qQA");
  // Check connection
     if($link === false){
     die("ERROR: Nao foi possivel conectar ao RDS " . mysqli_connect_error());
